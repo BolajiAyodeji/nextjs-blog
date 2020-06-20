38b4ae833290ee1723dc04b0f1fc335737fa49e5
bolajiayodeji <sirbeejay1@gmail.com>
2020-06-20 03:53:00 +0100

    Add users demo api

---
+++ pages/api/users.js
@@ -0,0 +1,9 @@
+export default (req, res) => {
+    res.status(200).json({ 
+        name: 'Bolaji Ayodeji',
+        email: 'bolaji@lorem.com',
+        telephone: "+234 5677664433",
+        balance: '2333USD'
+    })
+  }
+
