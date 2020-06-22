b7077b2d1d4d8d11abf0da8ec048a559b91566d1
bolajiayodeji <sirbeejay1@gmail.com>
2020-06-22 04:10:17 +0100

    del unused files

--- pages/api/users.js
+++
@@ -1,9 +0,0 @@
-export default (req, res) => {
-    res.status(200).json({ 
-        name: 'Bolaji Ayodeji',
-        email: 'bolaji@lorem.com',
-        telephone: "+234 5677664433",
-        balance: '2333USD'
-    })
-  }
-
