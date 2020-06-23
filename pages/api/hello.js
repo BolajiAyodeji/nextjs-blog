8e1c6d9c32c55262259b7599433fd05c17d1ba35
Bolaji Ayodeji <sirbeejay1@gmail.com>
2020-06-23 10:40:28 +0100

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,9 @@
 export default (req, res) => {
-    res.status(200).json({ text: 'Hello 👋' })
+    res.status(200).json({
+        text: 'Hello 👋'
+        descritpion: 'freeCodeCamp is a donor-supported tax-exempt 501(c)(3) nonprofit organization'
+        mission: 'to help people learn to code for free.'
+        cta: 'donations to freeCodeCamp go toward our education initiatives, and help pay for servers, services, and staff.'
+        donate: 'https://www.freecodecamp.org/donate'
+    })
   }
